cded33c213898719ce87f39a8545c13475b7321b
MaxRim <maxforsamp98@gmail.com>
2021-05-04 21:51:11 +0300

    Fix URI for Windows

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import reqparse
-from datetime import date
+import platform
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,7 +10,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
+# Windows URI different than Linux
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    ('sqlite:////', 'sqlite:///')[platform.system() == 'Windows'] + \
     os.path.join(basedir, 'courses.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
